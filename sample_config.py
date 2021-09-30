438a44143b1879acffd8f4100ad286bb390234e6
Crazy devil <85053195+kkverma25@users.noreply.github.com>
2021-09-30 07:51:27 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -18,13 +18,13 @@ class Config(object):
     API_HASH = "hmmmm"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 948247711  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "kavinduaj"
-    SUPPORT_CHAT = "lkhitech"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "YOUR_DEVIL_DAD"
+    SUPPORT_CHAT = "DEVIL_DAD_SUPPORT"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001553361246
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001553361246
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
